58c1140eff5b4eccf38edb8a0e02c80a05b3e3d4
Lokesh Kumar <lokesh2.yss@gmail.com>
2024-10-13 17:13:29 +0530

    Added Eureka client in User, API-Gateway and Post services

--- discovery-server/src/main/java/com/codingshuttle/linkedin/discovery_server/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/com/codingshuttle/linkedin/discovery_server/DiscoveryServerApplication.java
@@ -1,9 +1,13 @@
 package com.codingshuttle.linkedin.discovery_server;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class DiscoveryServerApplication {
 
 	public static void main(String[] args) {

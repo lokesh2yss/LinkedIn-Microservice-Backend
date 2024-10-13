68cf571a3e0576f119de637aaae1c7e5d7934cd3
Lokesh Kumar <lokesh2.yss@gmail.com>
2024-10-13 20:56:19 +0530

    Added connection service from handling connections between users in the linkedin app alson with neo4j database dependency

---
+++ connection-service/src/main/java/com/codingshuttle/linkedin/connection_service/ConnectionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.codingshuttle.linkedin.connection_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectionServiceApplication.class, args);
+	}
+
+}
---
+++ connection-service/src/test/java/com/codingshuttle/linkedin/connection_service/ConnectionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingshuttle.linkedin.connection_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
